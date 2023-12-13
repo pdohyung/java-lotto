0178920b0cda84c14b9dd6b55a02d0fc57001dbe
kimdohyung <pdohyung@gmail.com>
2023-12-14 01:56:39 +0900

    feat(Lotto): 로또 숫자가 1부터 45 사이의 숫자인지 검증

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,18 +1,33 @@
 package lotto.domain;
 
+import lotto.util.ErrorMessage;
+
 import java.util.List;
+
+import static lotto.util.Constants.*;
+import static lotto.util.ErrorMessage.INVALID_LOTTO_LENGTH;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateLottoLength(numbers);
+        validateLottoRange(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void validateLottoLength(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_LENGTH) {
+            throw new IllegalArgumentException(INVALID_LOTTO_LENGTH.getErrorMessage());
+        }
+    }
+
+    private void validateLottoRange(List<Integer> numbers) throws IllegalArgumentException {
+        boolean isValidRange = numbers.stream()
+                .allMatch(num -> num >= LOTTO_START_NUMBER && num <= LOTTO_END_NUMBER);
+
+        if (!isValidRange) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_NUMBER_RANGE.getErrorMessage());
         }
     }
 
--- src/main/java/lotto/util/ErrorMessage.java
+++ src/main/java/lotto/util/ErrorMessage.java
@@ -3,7 +3,9 @@ package lotto.util;
 public enum ErrorMessage {
     INVALID_NUMBER_FORMAT_MESSAGE("숫자 형식이 아닙니다."),
     INVALID_AMOUNT_MESSAGE("올바른 구입 금액이 아닙니다."),
-    INVALID_WINNING_NUMBERS_MESSAGE("올바른 당첨 번호가 아닙니다");
+    INVALID_WINNING_NUMBERS_MESSAGE("올바른 당첨 번호가 아닙니다"),
+    INVALID_LOTTO_LENGTH("로또 숫자는 6개입니다"),
+    INVALID_LOTTO_NUMBER_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
 
     private final String errorMessage;
 
