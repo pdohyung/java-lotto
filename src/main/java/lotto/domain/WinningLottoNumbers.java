c1c9214446032de5b5529e6173efa7f4bae93d6e
kimdohyung <pdohyung@gmail.com>
2023-12-14 14:07:16 +0900

    feat(WinningLottoNumbers): 보너스 번호의 범위와 당첨 번호와 중복되는지 검증

--- src/main/java/lotto/domain/WinningLottoNumbers.java
+++ src/main/java/lotto/domain/WinningLottoNumbers.java
@@ -1,12 +1,19 @@
 package lotto.domain;
 
+import lotto.util.ErrorMessage;
+
 import java.util.List;
+
+import static lotto.util.Constants.*;
+import static lotto.util.ErrorMessage.INVALID_LOTTO_NUMBER_DUPLICATE;
 
 public class WinningLottoNumbers {
     private final List<Integer> winningNumbers;
     private final int bonusNumber;
 
     public WinningLottoNumbers(List<Integer> winningNumbers, int bonusNumber) {
+        validateLottoRange(List.of(bonusNumber));
+        validateDuplicate(winningNumbers, bonusNumber);
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
     }
@@ -17,5 +24,20 @@ public class WinningLottoNumbers {
 
     public int getBonusNumber() {
         return bonusNumber;
+    }
+
+    private void validateDuplicate(List<Integer> numbers, int number) {
+        if (numbers.contains(number)) {
+            throw new IllegalArgumentException(INVALID_LOTTO_NUMBER_DUPLICATE.getErrorMessage());
+        }
+    }
+
+    private void validateLottoRange(List<Integer> numbers) throws IllegalArgumentException {
+        boolean isValidRange = numbers.stream()
+                .allMatch(num -> num >= LOTTO_START_NUMBER && num <= LOTTO_END_NUMBER);
+
+        if (!isValidRange) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_NUMBER_RANGE.getErrorMessage());
+        }
     }
 }
